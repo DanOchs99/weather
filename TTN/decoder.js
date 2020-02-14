501f5999332f3d45ca2e906d29178d10dd1cb60a
Dan Ochs <dochs@ochsholdings.com>
2020-02-14 15:57:09 -0600

    first commit

---
+++ TTN/decoder.js
@@ -0,0 +1,34 @@
+function Decoder(bytes, port) {
+    // Decode an uplink message from a buffer
+    // (array) of bytes to an object of fields.
+    var decoded = {};
+  
+    // Based on https://stackoverflow.com/a/37471538 by Ilya Bursov
+    function bytesToFloat(bytes) {
+      // JavaScript bitwise operators yield a 32 bit integer, not a float.
+      // Assume LSB (least significant byte first).
+      var bits = bytes[3]<<24 | bytes[2]<<16 | bytes[1]<<8 | bytes[0];
+      var sign = (bits>>>31 === 0) ? 1.0 : -1.0;
+      var e = bits>>>23 & 0xff;
+      var m = (e === 0) ? (bits & 0x7fffff)<<1 : (bits & 0x7fffff) | 0x800000;
+      var f = sign * m * Math.pow(2, e - 150);
+      return f;
+    }  
+        
+    // Decode temp and humidity on port 2
+    if (port === 2) {
+        // bytes 0 - 3 are temp reading (float)
+        decoded.temp = bytesToFloat(bytes.slice(0,4));
+        
+        // bytes 4 - 7 are humidity reading (float)
+        decoded.humidity = bytesToFloat(bytes.slice(4,8));
+    }
+    
+    // convert bytes to text on port 1; FOR TESTING ONLY
+    if (port === 1) {
+        decoded.msg = String.fromCharCode.apply(null, bytes);
+    }
+    
+    return decoded; 
+  }
+  
---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather Station</h1>
+        <div>temp</div>
+        <div>humidity</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
