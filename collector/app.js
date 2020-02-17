590779e48fc448805c1235672fe7fe68b4201c6d
Dan Ochs <dochs@ochsholdings.com>
2020-02-17 11:54:01 -0600

    split server between collector and web server

---
+++ collector/app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const ttn = require('ttn')
+
+const PORT = process.env.PORT
+
+const appID = process.env.APPID
+const accessKey = process.env.ACCESSKEY
+const dbURL = process.env.DATABASE_URL
+
+ttn.data(appID, accessKey)
+  .then((client) => { 
+      client.on("uplink", (devID, payload) => {
+          // TODO - send reading to database here!!
+          console.log("Received uplink from ",devID)
+          console.log(payload)
+      })
+      console.log("Listening for updates...")
+  })
+  .catch((err) => {
+      console.error("Unable to connect to TTN server.")
+      console.error(err)
+  })
--- server/app.js
+++ server/app.js
@@ -3,7 +3,30 @@ require("dotenv").config();
 
 const app = express()
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT
+const dbURL = process.env.DATABASE_URL
+
+app.get("/", (req,res) => {
+    res.json({"temp": "70.0", "humidity": "50"})
+})
+
+app.post("/login", (req,res) => {
+    // login a user, return a message and a token
+    
+
+})
+
+app.post("/register", (req,res) => {
+    // register a user, return a message and a token
+
+
+})
+
+app.post("/verify", (req,res) => {
+    // verify a token, return a message and the token
+
+
+})
 
 
 app.listen(PORT, ()=>{ 
